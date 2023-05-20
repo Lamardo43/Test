cf43b64075bba3c3f208c14674c170548bc40db8
Klim Medvedev <dater01@mail.ru>
2023-05-20 22:14:29 +0300

    we

---
+++ build-untitled-Desktop_Qt_6_4_2_MinGW_64_bit-Debug/debug/moc_mainwindow.cpp
@@ -0,0 +1,322 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'mainwindow.h'
+**
+** Created by: The Qt Meta Object Compiler version 68 (Qt 6.4.2)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include <memory>
+#include "../../university_project_client/mainwindow.h"
+#include <QtNetwork/QSslPreSharedKeyAuthenticator>
+#include <QtNetwork/QSslError>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'mainwindow.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 68
+#error "This file was generated using the moc from 6.4.2. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+#ifndef Q_CONSTINIT
+#define Q_CONSTINIT
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+namespace {
+struct qt_meta_stringdata_MainWindow_t {
+    uint offsetsAndSizes[52];
+    char stringdata0[11];
+    char stringdata1[15];
+    char stringdata2[1];
+    char stringdata3[21];
+    char stringdata4[10];
+    char stringdata5[4];
+    char stringdata6[8];
+    char stringdata7[8];
+    char stringdata8[18];
+    char stringdata9[13];
+    char stringdata10[27];
+    char stringdata11[32];
+    char stringdata12[11];
+    char stringdata13[22];
+    char stringdata14[16];
+    char stringdata15[21];
+    char stringdata16[33];
+    char stringdata17[36];
+    char stringdata18[32];
+    char stringdata19[10];
+    char stringdata20[24];
+    char stringdata21[35];
+    char stringdata22[17];
+    char stringdata23[5];
+    char stringdata24[39];
+    char stringdata25[5];
+};
+#define QT_MOC_LITERAL(ofs, len) \
+    uint(sizeof(qt_meta_stringdata_MainWindow_t::offsetsAndSizes) + ofs), len 
+Q_CONSTINIT static const qt_meta_stringdata_MainWindow_t qt_meta_stringdata_MainWindow = {
+    {
+        QT_MOC_LITERAL(0, 10),  // "MainWindow"
+        QT_MOC_LITERAL(11, 14),  // "open_user_info"
+        QT_MOC_LITERAL(26, 0),  // ""
+        QT_MOC_LITERAL(27, 20),  // "about_user_info_send"
+        QT_MOC_LITERAL(48, 9),  // "slot_show"
+        QT_MOC_LITERAL(58, 3),  // "log"
+        QT_MOC_LITERAL(62, 7),  // "update1"
+        QT_MOC_LITERAL(70, 7),  // "update2"
+        QT_MOC_LITERAL(78, 17),  // "button_is_pressed"
+        QT_MOC_LITERAL(96, 12),  // "QPushButton*"
+        QT_MOC_LITERAL(109, 26),  // "on_pushButton_Test_clicked"
+        QT_MOC_LITERAL(136, 31),  // "on_listWidget_currentRowChanged"
+        QT_MOC_LITERAL(168, 10),  // "currentRow"
+        QT_MOC_LITERAL(179, 21),  // "on_pushButton_clicked"
+        QT_MOC_LITERAL(201, 15),  // "slot_on_auth_ok"
+        QT_MOC_LITERAL(217, 20),  // "slot_on_auth_invalid"
+        QT_MOC_LITERAL(238, 32),  // "on_pushButton_copy_login_clicked"
+        QT_MOC_LITERAL(271, 35),  // "on_pushButton_copy_password_c..."
+        QT_MOC_LITERAL(307, 31),  // "on_pushButton_user_info_clicked"
+        QT_MOC_LITERAL(339, 9),  // "user_exit"
+        QT_MOC_LITERAL(349, 23),  // "prepare_window_for_user"
+        QT_MOC_LITERAL(373, 34),  // "on_listWidget_services_itemCl..."
+        QT_MOC_LITERAL(408, 16),  // "QListWidgetItem*"
+        QT_MOC_LITERAL(425, 4),  // "item"
+        QT_MOC_LITERAL(430, 38),  // "on_lineEdit_services_search_t..."
+        QT_MOC_LITERAL(469, 4)   // "arg1"
+    },
+    "MainWindow",
+    "open_user_info",
+    "",
+    "about_user_info_send",
+    "slot_show",
+    "log",
+    "update1",
+    "update2",
+    "button_is_pressed",
+    "QPushButton*",
+    "on_pushButton_Test_clicked",
+    "on_listWidget_currentRowChanged",
+    "currentRow",
+    "on_pushButton_clicked",
+    "slot_on_auth_ok",
+    "slot_on_auth_invalid",
+    "on_pushButton_copy_login_clicked",
+    "on_pushButton_copy_password_clicked",
+    "on_pushButton_user_info_clicked",
+    "user_exit",
+    "prepare_window_for_user",
+    "on_listWidget_services_itemClicked",
+    "QListWidgetItem*",
+    "item",
+    "on_lineEdit_services_search_textEdited",
+    "arg1"
+};
+#undef QT_MOC_LITERAL
+} // unnamed namespace
+
+Q_CONSTINIT static const uint qt_meta_data_MainWindow[] = {
+
+ // content:
+      10,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      18,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       2,       // signalCount
+
+ // signals: name, argc, parameters, tag, flags, initial metatype offsets
+       1,    0,  122,    2, 0x06,    1 /* Public */,
+       3,    2,  123,    2, 0x06,    2 /* Public */,
+
+ // slots: name, argc, parameters, tag, flags, initial metatype offsets
+       4,    1,  128,    2, 0x0a,    5 /* Public */,
+       6,    0,  131,    2, 0x08,    7 /* Private */,
+       7,    0,  132,    2, 0x08,    8 /* Private */,
+       8,    1,  133,    2, 0x08,    9 /* Private */,
+      10,    0,  136,    2, 0x08,   11 /* Private */,
+      11,    1,  137,    2, 0x08,   12 /* Private */,
+      13,    0,  140,    2, 0x08,   14 /* Private */,
+      14,    1,  141,    2, 0x08,   15 /* Private */,
+      15,    0,  144,    2, 0x08,   17 /* Private */,
+      16,    0,  145,    2, 0x08,   18 /* Private */,
+      17,    0,  146,    2, 0x08,   19 /* Private */,
+      18,    0,  147,    2, 0x08,   20 /* Private */,
+      19,    0,  148,    2, 0x08,   21 /* Private */,
+      20,    0,  149,    2, 0x08,   22 /* Private */,
+      21,    1,  150,    2, 0x08,   23 /* Private */,
+      24,    1,  153,    2, 0x08,   25 /* Private */,
+
+ // signals: parameters
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::QString, QMetaType::QString,    2,    2,
+
+ // slots: parameters
+    QMetaType::Void, QMetaType::QString,    5,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, 0x80000000 | 9,    2,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Int,   12,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::QString,    2,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, 0x80000000 | 22,   23,
+    QMetaType::Void, QMetaType::QString,   25,
+
+       0        // eod
+};
+
+Q_CONSTINIT const QMetaObject MainWindow::staticMetaObject = { {
+    QMetaObject::SuperData::link<QMainWindow::staticMetaObject>(),
+    qt_meta_stringdata_MainWindow.offsetsAndSizes,
+    qt_meta_data_MainWindow,
+    qt_static_metacall,
+    nullptr,
+    qt_incomplete_metaTypeArray<qt_meta_stringdata_MainWindow_t,
+        // Q_OBJECT / Q_GADGET
+        QtPrivate::TypeAndForceComplete<MainWindow, std::true_type>,
+        // method 'open_user_info'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'about_user_info_send'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        QtPrivate::TypeAndForceComplete<QString, std::false_type>,
+        QtPrivate::TypeAndForceComplete<QString, std::false_type>,
+        // method 'slot_show'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        QtPrivate::TypeAndForceComplete<QString, std::false_type>,
+        // method 'update1'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'update2'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'button_is_pressed'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        QtPrivate::TypeAndForceComplete<QPushButton *, std::false_type>,
+        // method 'on_pushButton_Test_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_listWidget_currentRowChanged'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        QtPrivate::TypeAndForceComplete<int, std::false_type>,
+        // method 'on_pushButton_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'slot_on_auth_ok'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        QtPrivate::TypeAndForceComplete<QString, std::false_type>,
+        // method 'slot_on_auth_invalid'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_pushButton_copy_login_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_pushButton_copy_password_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_pushButton_user_info_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'user_exit'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'prepare_window_for_user'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_listWidget_services_itemClicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        QtPrivate::TypeAndForceComplete<QListWidgetItem *, std::false_type>,
+        // method 'on_lineEdit_services_search_textEdited'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        QtPrivate::TypeAndForceComplete<const QString &, std::false_type>
+    >,
+    nullptr
+} };
+
+void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<MainWindow *>(_o);
+        (void)_t;
+        switch (_id) {
+        case 0: _t->open_user_info(); break;
+        case 1: _t->about_user_info_send((*reinterpret_cast< std::add_pointer_t<QString>>(_a[1])),(*reinterpret_cast< std::add_pointer_t<QString>>(_a[2]))); break;
+        case 2: _t->slot_show((*reinterpret_cast< std::add_pointer_t<QString>>(_a[1]))); break;
+        case 3: _t->update1(); break;
+        case 4: _t->update2(); break;
+        case 5: _t->button_is_pressed((*reinterpret_cast< std::add_pointer_t<QPushButton*>>(_a[1]))); break;
+        case 9: _t->slot_on_auth_ok((*reinterpret_cast< std::add_pointer_t<QString>>(_a[1]))); break;
+        case 10: _t->slot_on_auth_invalid(); break;
+        case 11: _t->on_pushButton_copy_login_clicked(); break;
+        case 12: _t->on_pushButton_copy_password_clicked(); break;
+        case 13: _t->on_pushButton_user_info_clicked(); break;
+        case 14: _t->user_exit(); break;
+        case 15: _t->prepare_window_for_user(); break;
+        case 16: _t->on_listWidget_services_itemClicked((*reinterpret_cast< std::add_pointer_t<QListWidgetItem*>>(_a[1]))); break;
+        case 17: _t->on_lineEdit_services_search_textEdited((*reinterpret_cast< std::add_pointer_t<QString>>(_a[1]))); break;
+        default: ;
+        }
+    } else if (_c == QMetaObject::IndexOfMethod) {
+        int *result = reinterpret_cast<int *>(_a[0]);
+        {
+            using _t = void (MainWindow::*)();
+            if (_t _q_method = &MainWindow::open_user_info; *reinterpret_cast<_t *>(_a[1]) == _q_method) {
+                *result = 0;
+                return;
+            }
+        }
+        {
+            using _t = void (MainWindow::*)(QString , QString );
+            if (_t _q_method = &MainWindow::about_user_info_send; *reinterpret_cast<_t *>(_a[1]) == _q_method) {
+                *result = 1;
+                return;
+            }
+        }
+    }
+}
+
+const QMetaObject *MainWindow::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *MainWindow::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_MainWindow.stringdata0))
+        return static_cast<void*>(this);
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 18)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 18;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 18)
+            *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType();
+        _id -= 18;
+    }
+    return _id;
+}
+
+// SIGNAL 0
+void MainWindow::open_user_info()
+{
+    QMetaObject::activate(this, &staticMetaObject, 0, nullptr);
+}
+
+// SIGNAL 1
+void MainWindow::about_user_info_send(QString _t1, QString _t2)
+{
+    void *_a[] = { nullptr, const_cast<void*>(reinterpret_cast<const void*>(std::addressof(_t1))), const_cast<void*>(reinterpret_cast<const void*>(std::addressof(_t2))) };
+    QMetaObject::activate(this, &staticMetaObject, 1, _a);
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
